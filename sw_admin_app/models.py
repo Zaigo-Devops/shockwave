1ea932579838cb757a90294825f5dc374ff5d0ae
abithiyagu24 <abinayathiyagarajan20@gamil.com>
2023-04-27 11:12:13 +0530

    Device price retrive from admin panel had done

--- sw_admin_app/models.py
+++ sw_admin_app/models.py
@@ -131,6 +131,8 @@ class SubscriptionPeriod(models.Model):
 
 class DevicePrice(models.Model):
     price = models.FloatField(default=0.0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.price)
